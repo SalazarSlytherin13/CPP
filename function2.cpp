37d6884527d5742418c807e65ccca7fefb9a63e3
SalazarSlytherin13 <vanyatripathig1484@gmail.com>
2026-07-29 09:49:37 +0530

    function2.cpp

---
+++ function2.cpp
@@ -0,0 +1,26 @@
+// Sum of two numbers using Pass By Address
+
+#include<iostream>
+using namespace std;
+
+// Function Declaration
+int sum(int *, int *);
+
+// Caller Function
+int main(){
+    int a,b,r;
+    cout<<"Enter the value: "<<"\n";
+    cin>>a>>b;
+    cout<<&a<<"\n";
+    cout<<&b<<"\n";
+    r=sum(&a,&b);   
+    cout<<r<<"\n";
+    return 0;
+}
+
+// Function Definition
+int sum(int *x, int *y){
+    int s= *x + *y;
+    return s;
+}
+
