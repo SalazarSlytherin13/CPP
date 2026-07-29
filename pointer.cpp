cbd4ab903e756f771b4bfb405b3fb0ce41745227
SalazarSlytherin13 <vanyatripathig1484@gmail.com>
2026-07-29 10:31:28 +0530

    pointer.cpp

--- pointer.cpp
+++ pointer.cpp
@@ -3,6 +3,12 @@ using namespace std;
 
 int main(){
     int a=10;
+    cout<<sizeof(char)<<"\n";
+    cout<<sizeof(float)<<"\n";    
+    cout<<sizeof(double)<<"\n";
+    cout<<sizeof(long)<<"\n";
+    cout<<sizeof(long long)<<"\n";
+    cout<<sizeof(a)<<"\n";
     int *p = &a;
     cout<<a<<"\n";
     cout<<*(&a)<<"\n";
